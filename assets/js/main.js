5399243e41066a927d51d930944347638f51d658
VannPellegrini <vanessapellegrini@hotmail.com>
2017-07-07 22:42:32 -0400

    cambios y pruebas js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,79 @@
 /*alert("hola");*/
 $(document).ready(function() {
-
-    for (var i = 1; i < 20; i++) {
-        var pokePic = $("<img src=http://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
-        $('div.pokemon').append(pokePic);
-    }
-
-    $('img', this).click(function() {
-
-        var uniqueId = $(this).attr('id');
-
-        var webAddress = "http://pokeapi.co/api/v1/pokemon/" + uniqueId;
-
-        var types = " ";
-
-        $.get(webAddress, function(res) {
-            for (var j = 0; j < res.types.length; j++) {
-                types += "<li>" + res.types[j].name + "</li>";
-
-            }
-        }, 'json');
-
-        $.get(webAddress, function(res) {
-
-            $('div.pokedex').html(
-                "<h1>" + res.name + "</h1>" +
-                "<img src=http://pokeapi.co/media/img/" + uniqueId + ".png>" +
-                "<h4>Types:</h4>" +
-                "<ul>" +
-                types +
-                "</ul>" +
-                "<h4>Height:</h4>" +
-                "<p>" + res.height + "</p>" +
-                "<h4>Weight:</h4>" +
-                "<p>" + res.weight + "</p>"
-            );
-
-        }, 'json');
-    });
+    pokemon();
 });
+
+//obetener pokemones
+function pokemon(){
+    //recorro las imagenes
+    for(var id = 1; id <= 719; id++){
+        $("#pokemon").append("<img id=pokeID"+id+" pokeID='"+id+"' src='http://pokeapi.co/media/img/"+id+".png' alt='Pokemon # "+id+" from pokeapi.com'>");
+    }
+    var pokeId = 1;
+    /*obtengo datos ajax para busqueda
+    var allPoke = (function(){
+        return $.ajax({
+            url: 'http://pokeapi.co/api/v2/pokemon/',
+            dataType: 'JSON',
+            method: 'GET',
+            //data: {'limit': '20'}
+        });
+    });*/
+    $.get("http://pokeapi.co/api/v2/pokemon/"+pokeId+"/", function(pokeInfo) {
+        var pokeName = pokeInfo.name;
+        $("#pokename").append("<h2>"+pokeInfo.name+"</h2>");
+        $("#mainpokeimage").attr("src","http://pokeapi.co/media/img/"+pokeId+".png");
+        for(var i = 0; i < pokeInfo.types.length; i++){
+            $("#poketypes").append("<li>"+pokeInfo.types[i].name+"</li>");
+        };
+        $("#height").append("<p>"+pokeInfo.height+"</p>");
+        $("#weight").append("<p>"+pokeInfo.weight+"</p>");
+        for(var i = 0; i < pokeInfo.abilities.length; i++){
+            $("#abilities").append("<p>"+pokeInfo.abilities[i].name+"</p>");
+        };
+        $("#attackdefense").append("<p>"+pokeInfo.attack+" | "+pokeInfo.defense+"</p>");
+    }, "json");                
+};
+//al hacer click al pokemon actualiza el html y muestra la info
+$(document).on("click", "img", function(){
+    //usa el mismo id que la funcion pokemon
+    var pokeId = $(this).attr("id");
+    //obtengo info de la API
+    $.get("http://pokeapi.co/api/v2/pokemon/"+pokeId+"/", function(pokeInfo) {
+        
+        var pokeId = pokeInfo.name;
+            $("h2").remove;    // why is this line not working? AHHHHHHHHHHHH.
+            $("#pokename").append("<h2>"+pokeInfo.name+"</h2>");
+            $("#mainpokeimage").attr("src","http://pokeapi.co/media/img/"+pokeId+".png");
+            for(var i = 0; i < pokeInfo.types.length; i++){
+                $("#poketypes").append("<li>"+pokeInfo.types[i].name+"</li>");
+            }
+            $("#height").append("<p>"+pokeInfo.height+"</p>");
+            $("#weight").append("<p>"+pokeInfo.weight+"</p>");
+            for(var i = 0; i < pokeInfo.abilities.length; i++){
+                console.log(pokeInfo.abilities[i].name);
+                $("#abilities").append("<p>"+pokeInfo.abilities[i].name+"</p>");
+            }
+            $("#attackdefense").append("<p>"+pokeInfo.attack+" | "+pokeInfo.defense+"</p>");
+            }, "json");                
+        });
+ /*
+
+    /*imagenes solo toma 719 son 811
+    var obtenerImg = (function(){
+        for (var i = 1; i < 719 ; i++) {
+            var pokeImg = $("<img src=http://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+            $('div.pokemon').append(pokeImg);
+        };
+    });
+    /*var datosPokemon = function () {
+    $.getJSON("https://pokeapi.co/api/v2/pokemon/",
+        function (response) {
+            var pokemon = response.results;
+            crearPokemons(pokemon);
+            console.log(pokemon);
+            $(".modal").modal();
+            $('.contenedorModal').click(masInformacion);
+        });
+    };*/
+
